97c45f49b472e67309203190644bec13913d1cb1
Federico Sabiche <fsabiche>
2023-11-30 21:58:38 -0300

    imagenes y parte del footer

---
+++ Front/src/components/Footer.jsx
@@ -0,0 +1,106 @@
+
+
+const Footer = () => {
+  return (
+
+<section className="py-10 bg-w sm:py-16 lg:py-24 z-40 relative">
+
+
+    <div className="container mx-auto">
+        <h2 className="text-3xl font-light   text-black sm:text-4xl lg:text-5xl">
+           <span className="block w-full font-light text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-green-500 lg:inline">Nuestras 
+            </span> propiedades disponibles.
+        </h2>
+        <p className="mb-20 text-lg text-gray-900">Comes directly from the desk of engineers, creators and managers at
+            Skcript.
+        </p>
+
+        <div className="grid grid-cols-1 gap-6 lg:grid-cols-6 ">
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Destacados</h2>
+                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example "/>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Lanzamientos</h2>
+                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example "/>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Propietarios</h2>
+                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example "/>
+                </div>
+            </a> 
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Nuevos</h2>
+                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example"/>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Mobiliados</h2>
+                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example"/>
+                </div>
+            </a>
+            <a href="#" className=" shadow-2xl relative ">
+                <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                    <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                        <div className="w-full h-full   p-5   relative">
+                            <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-lefttransition-all ease-in-out duration-500 ">
+                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Temporales</h2>
+                                <p className="text-lg font-light text-white">Temporales</p>
+                            </div>
+                        </div>
+                    </div>
+                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full object-fill example"/>
+                </div>
+            </a>
+
+
+        </div>
+
+</div>
+</section>
+ 
+  )
+}
+export default Footer
