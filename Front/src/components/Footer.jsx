d435bbb34c61c12587e3b356984e22734e35183c
Federico Sabiche <fsabiche>
2023-12-01 18:44:33 -0300

    se finalizo cards de imagenes de propiedades del footer

--- Front/src/components/Footer.jsx
+++ Front/src/components/Footer.jsx
@@ -1,9 +1,16 @@
+import imagen1 from '../assets/img/bernard-1.jpg'
+import imagen2 from '../assets/img/chris-curry-1.jpg'
+import imagen3 from '../assets/img/chris-curry-2.jpg'
+import imagen4 from '../assets/img/fomstock-4.jpg'
+import imagen5 from '../assets/img/gus-ruballo-1.jpg'
+import imagen6 from '../assets/img/yaopey-1.jpg'
+
 
 
 const Footer = () => {
   return (
 
-<section className="py-10 bg-w sm:py-16 lg:py-24 z-40 relative">
+<section className="py-10 bg-w m-4 sm:py-16 lg:py-24 z-40 relative">
 
 
     <div className="container mx-auto">
@@ -11,7 +18,7 @@ const Footer = () => {
            <span className="block w-full font-light text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-green-500 lg:inline">Nuestras 
             </span> propiedades disponibles.
         </h2>
-        <p className="mb-20 text-lg text-gray-900">Comes directly from the desk of engineers, creators and managers at
+        <p className="mb-20 text-lg text-gray-900 mt-2">Comes directly from the desk of engineers, creators and managers at
             Skcript.
         </p>
 
@@ -21,12 +28,13 @@ const Footer = () => {
                     <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
                         <div className="w-full h-full   p-5   relative">
                             <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
-                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Destacados</h2>
-                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                                <h2 className="text-2xl font-bold  text-white mb-3 pb-3">Destacados</h2>
+                                <p className="text-lg font-light text-white">Destacados</p>
                             </div>
                         </div>
                     </div>
-                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example "/>
+                    <img src={imagen1} className="w-full h-full object-fill object-center justify-items-stretch" />
+
                 </div>
             </a>
             <a href="#" className=" shadow-2xl relative ">
@@ -34,12 +42,12 @@ const Footer = () => {
                     <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
                         <div className="w-full h-full   p-5   relative">
                             <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
-                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Lanzamientos</h2>
-                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                                <h2 className="text-2xl font-bold  text-white mb-3 pb-3">Lanzamientos</h2>
+                                <p className="text-lg font-light text-white">Lanzamientos</p>
                             </div>
                         </div>
                     </div>
-                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example "/>
+                    <img src={imagen2} className="w-full z-0  h-full    object-fill example "/>
                 </div>
             </a>
             <a href="#" className=" shadow-2xl relative ">
@@ -47,12 +55,12 @@ const Footer = () => {
                     <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
                         <div className="w-full h-full   p-5   relative">
                             <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
-                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Propietarios</h2>
-                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                                <h2 className="text-2xl font-bold  text-white mb-3 pb-3">Propietarios</h2>
+                                <p className="text-lg font-light text-white">propietarios</p>
                             </div>
                         </div>
                     </div>
-                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example "/>
+                    <img src={imagen3} className="w-full z-0  h-full    object-fill example "/>
                 </div>
             </a> 
             <a href="#" className=" shadow-2xl relative ">
@@ -60,12 +68,12 @@ const Footer = () => {
                     <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
                         <div className="w-full h-full   p-5   relative">
                             <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
-                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Nuevos</h2>
-                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                                <h2 className="text-2xl font-bold  text-white mb-3 pb-3">Nuevos</h2>
+                                <p className="text-lg font-light text-white">Nuevos</p>
                             </div>
                         </div>
                     </div>
-                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example"/>
+                    <img src={imagen4} className="w-full z-0  h-full    object-fill example"/>
                 </div>
             </a>
             <a href="#" className=" shadow-2xl relative ">
@@ -73,12 +81,12 @@ const Footer = () => {
                     <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
                         <div className="w-full h-full   p-5   relative">
                             <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
-                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Mobiliados</h2>
-                                <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                                <h2 className="text-2xl font-bold  text-white mb-3 pb-3">Mobiliados</h2>
+                                <p className="text-lg font-light text-white">Mobiliados</p>
                             </div>
                         </div>
                     </div>
-                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example"/>
+                    <img src={imagen5} className="w-full z-0  h-full    object-fill example"/>
                 </div>
             </a>
             <a href="#" className=" shadow-2xl relative ">
@@ -86,12 +94,12 @@ const Footer = () => {
                     <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
                         <div className="w-full h-full   p-5   relative">
                             <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-lefttransition-all ease-in-out duration-500 ">
-                                <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Temporales</h2>
+                                <h2 className="text-2xl font-bold  text-white  mb-3 pb-3">Temporales</h2>
                                 <p className="text-lg font-light text-white">Temporales</p>
                             </div>
                         </div>
                     </div>
-                    <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full object-fill example"/>
+                    <img src={imagen6} className="w-full z-0  h-full object-fill example"/>
                 </div>
             </a>
 
