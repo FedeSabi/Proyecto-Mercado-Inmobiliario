375fe633f609d03665886ede9c93efebc9a4d5cb
Federico Sabiche <fsabiche>
2023-12-05 12:06:07 -0300

    se cambio el color de los form

--- Front/src/pages/FormCorretor.jsx
+++ Front/src/pages/FormCorretor.jsx
@@ -1,12 +1,13 @@
+
 const FormCorretor = () => {
   return (
-    <div className="bg-green-100 dark:bg-green-800 transition-colors duration-300">
+    <div className="bg-white dark:bg-orange-700 transition-colors duration-300">
       <div className="container mx-auto p-4">
-        <div className="bg-white dark:bg-green-700 shadow rounded-lg p-6">
-          <h1 className="text-xl font-semibold mb-4 text-green-600 dark:text-green-100">
+        <div className="bg-white dark:bg-orange-700 shadow rounded-lg p-6">
+          <h1 className="text-xl font-semibold mb-4 text-black-700 dark:text-white">
             Construtora
           </h1>
-          <p className="text-green-600 dark:text-green-300 mb-6">
+          <p className="text-black-700 dark:text-orange-700 mb-6">
             Utiliza una dirección permanente donde puedas recibir correo.
           </p>
           <form>
@@ -14,59 +15,59 @@ const FormCorretor = () => {
               <input
                 type="text"
                 placeholder="Instagram"
-                className="border p-2 rounded w-full border-green-500"
+                className="border p-2 rounded w-full border-orange-600"
               />
               <input
                 type="text"
                 placeholder="Facebook"
-                className="border p-2 rounded w-full  border-green-500 "
+                className="border p-2 rounded w-full border-orange-600" 
               />
               <input
                 type="text"
                 placeholder="CNPJ"
-                className="border p-2 rounded w-full border-green-500"
+                className="border p-2 rounded w-full border-orange-600"
               />
               <input
                 type="text"
                 placeholder="Creci"
-                className="border p-2 rounded w-full border-green-500"
+                className="border p-2 rounded w-full border-orange-600"
               />
               <input
                 type="text"
                 placeholder="Ciudad"
-                className="border p-2 rounded w-full border-green-500"
+                className="border p-2 rounded w-full border-orange-600"
               />
               <input
                 type="text"
                 placeholder="Endereco"
-                className="border p-2 rounded w-full border-green-500"
+                className="border p-2 rounded w-full border-orange-600"
               />
               <input
                 type="text"
                 placeholder="Descripcion"
-                className="border p-2 rounded w-full border-green-500"
+                className="border p-2 rounded w-full border-orange-600"
               />
               <input
                 type="number"
                 placeholder="Telefono"
-                className="border p-2 rounded w-full border-green-500"
+                className="border p-2 rounded w-full border-orange-600"
               />
               <input
                 type="text"
                 placeholder="Inmobiliaria"
-                className="border p-2 rounded w-full border-green-500"
+                className="border p-2 rounded w-full border-orange-600"
               />
               <input
                 type="text"
                 placeholder="web"
-                className="border p-2 rounded w-full border-green-500"
+                className="border p-2 rounded w-full border-orange-600"
               />
               <div>
-                <p className="mb-3 text-green-600 text-md">Subir Imagenes</p>
+                <p className="mb-3 text-black-700 text-md">Subir Imagenes</p>
                 <input
                   type="file"
                   placeholder="subir imagenes"
-                  className="border p-2 rounded w-full border-green-500"
+                  className="border p-2 rounded w-full border-orange-600"
                 />
               </div>
             </div>
@@ -75,7 +76,7 @@ const FormCorretor = () => {
                 <input
                   type="checkbox"
                   id="remember"
-                  className="w-4 h-4 border-slate-200 focus:bg-green-900"
+                  className="w-4 h-4 border-slate-200 focus:bg-orange-600"
                 />
                 Autorizo a mostrar sus datos de contaro en las redes
               </label>
@@ -83,7 +84,7 @@ const FormCorretor = () => {
             <button
               type="button"
               id="theme-toggle"
-              className="px-4 py-2 rounded bg-green-900 text-white hover:bg-green-500 focus:outline-none transition-colors"
+              className="px-4 py-2 rounded bg-orange-600 text-white hover:bg-orange-300 focus:outline-none transition-colors"
             >
               Enviar
             </button>
