1a67029ebd50a23bf03b4e9e94fdae4e0e8247d3
Federico Sabiche <fsabiche>
2024-01-03 14:17:18 -0300

    formulario propiedades

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home.jsx";
 import Footer from "./components/Footer.jsx";
 import CardsPropiedades from "./pages/CardsPropiedades.jsx";
 import FormUsuario from "./pages/FormUsuario.jsx";
+import InmuebleFormPersona from "./pages/InmuebleFormPersona.jsx";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/FormUsuario" element={<FormUsuario />} />
           <Route path="/FormCorretor" element={<FormCorretor />} />
           <Route path="/CardsPropiedades" element={<CardsPropiedades />} />
+          <Route path="/InmuebleFormPersona" element={<InmuebleFormPersona />} />
         </Routes>
         <Footer />
       </Router>
