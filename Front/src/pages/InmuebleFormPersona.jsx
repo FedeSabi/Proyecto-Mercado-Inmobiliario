0c34069f61a9cf888acc80399a132c2334eb3e4f
Federico Sabiche <fsabiche>
2024-01-03 20:18:07 -0300

    me modifico remember me

--- Front/src/pages/InmuebleFormPersona.jsx
+++ Front/src/pages/InmuebleFormPersona.jsx
@@ -105,7 +105,7 @@ const InmuebleFormPersona = () => {
           </select>
           
         </div>
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 justify-between">
         <div>
             <label htmlFor="remember" className="">
               <input
