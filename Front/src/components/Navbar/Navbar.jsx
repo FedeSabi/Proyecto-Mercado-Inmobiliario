bb115c7904094f0fb61764e88c5c110750f5a96e
Federico Sabiche <fsabiche>
2024-02-07 16:44:39 -0300

    se modifico button al front y se agregaron archivos en back

--- Front/src/components/Navbar/Navbar.jsx
+++ Front/src/components/Navbar/Navbar.jsx
@@ -1,89 +1,177 @@
-import React from 'react';
-import { useState } from 'react';
+import { useState } from "react";
 import { FaFacebook, FaInstagram } from "react-icons/fa";
 import { BsPersonCircle } from "react-icons/bs";
 import { BsWhatsapp } from "react-icons/bs";
-import FormLogin from '../../pages/FormLogin';
-import {Link} from 'react-router-dom'
+import FormLogin from "../../pages/FormLogin";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    const [loginVisible, setLoginVisible] = useState(false);
+  const [loginVisible, setLoginVisible] = useState(false);
 
-    const toggleLogin = () => {
-      setLoginVisible(!loginVisible);
-      
-    };
-    
-    
-      const [menuVisible, setMenuVisible] = useState(false);
-    
-      const toggleMenu = () => {
-        setMenuVisible(!menuVisible);
-      };
-    return(
-    <div >
-             <nav className={`flex justify-between text-white bg-gray-900 bg-opacity-30 w-screen p-4 z-50 fixed ${menuVisible ? 'flex-col' : ''}`}>
+  const toggleLogin = () => {
+    setLoginVisible(!loginVisible);
+  };
 
-<div className="px-5 xl:px-12 py-2 flex w-full items-center">
-  <a className="text-3xl font-bold font-heading" href="/">
-    <p className="text-orange-500">Mercado</p>
-    <p className="h-9 text-orange-500">Inmobiliario</p>
-  </a>
+  const [menuVisible, setMenuVisible] = useState(false);
 
-  <ul className={`hidden md:flex px-6 mx-auto font-semibold font-heading space-x-12 ${menuVisible ? 'hidden' : 'flex'}`}>
-  <li><a className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl" href="/CardsPropiedades">Inmuebles</a></li>
-<li><a className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl" href="/FormCorretor">Corredores</a></li>
-<li><a className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl" href="/FormConstrutora">Constructoras</a></li>
-<li><a className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl" href="#">Nosotros</a></li>
-<li><a className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl" href="#">Contactos</a></li>
-<li><a className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl" href="#">Blog</a></li>
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+  return (
+    <div>
+      <nav
+        className={`flex justify-between text-white bg-gray-900 bg-opacity-30 w-screen p-4 z-50 fixed ${
+          menuVisible ? "flex-col" : ""
+        }`}
+      >
+        <div className="px-5 xl:px-12 py-2 flex w-full items-center">
+          <a className="text-3xl font-bold font-heading" href="/">
+            <p className="text-orange-500">Mercado</p>
+            <p className="h-9 text-orange-500">Inmobiliario</p>
+          </a>
 
-  </ul>
+          <ul
+            className={`hidden md:flex px-6 mx-auto font-semibold font-heading space-x-12 ${
+              menuVisible ? "hidden" : "flex"
+            }`}
+          >
+            <li>
+              <a
+                className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl"
+                href="/CardsPropiedades"
+              >
+                Inmuebles
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl"
+                href="/FormCorretor"
+              >
+                Corredores
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl"
+                href="/FormConstrutora"
+              >
+                Constructoras
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl"
+                href="#"
+              >
+                Nosotros
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl"
+                href="#"
+              >
+                Contactos
+              </a>
+            </li>
+            <li>
+              <a
+                className="hover:text-gray-200 text-sm md:text-lg lg:text-xl xl:text-1xl"
+                href="#"
+              >
+                Blog
+              </a>
+            </li>
+          </ul>
 
-  <div className="hidden xl:flex items-center space-x-5 items-center">
-    <a className="hover:text-gray-500 text-orange-500" href="#">
-      <FaFacebook size={30} />
-    </a>
-    <a className="flex items-center hover:text-gray-500 text-orange-500" href="#">
-      <FaInstagram size={30} />
-    </a>
-    <a className="flex items-center hover:text-gray-500 text-orange-500" href="#">
-      <BsWhatsapp size={30} />
-    </a>
-    <a className="flex items-center hover:text-gray-500 text-orange-500" href="#" onClick={toggleLogin}>
-    <BsPersonCircle size={30}/>
-</a>
+          <div className="hidden xl:flex items-center space-x-5 items-center">
+            <a className="hover:text-gray-500 text-orange-500" href="#">
+              <FaFacebook size={30} />
+            </a>
+            <a
+              className="flex items-center hover:text-gray-500 text-orange-500"
+              href="#"
+            >
+              <FaInstagram size={30} />
+            </a>
+            <a
+              className="flex items-center hover:text-gray-500 text-orange-500"
+              href="#"
+            >
+              <BsWhatsapp size={30} />
+            </a>
+            <a
+              className="flex items-center hover:text-gray-500 text-orange-500"
+              href="#"
+              onClick={toggleLogin}
+            >
+              <BsPersonCircle size={30} />
+            </a>
+          </div>
+        </div>
 
+        <a
+          className="navbar-burger self-center mr-12 xl:hidden"
+          href="#"
+          onClick={toggleMenu}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-12 hover:text-gray-200"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </a>
 
-  </div>
-</div>
-
-<a className="navbar-burger self-center mr-12 xl:hidden" href="#" onClick={toggleMenu}>
-  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-12 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-  </svg>
-</a>
-
-<ul className={`md:hidden px-6 mx-auto font-semibold font-heading  ${menuVisible ? 'flex flex-col' : 'hidden'}`} >
-  <li>
-  <Link to="/Inmuebles"className='hover:text-gray-200'>Inmueble</Link>
-  </li>
-  <li><a className="hover:text-gray-200 " href="#">Ver corredores</a></li>
-  <li><a className="hover:text-gray-200 " href="#">Ver constructoras</a></li>
-  <li><a className="hover:text-gray-200 " href="#">Sobre nosotros</a></li>
-  <li><a className="hover:text-gray-200 " href="#">Contactos</a></li>
-  <li><a className="hover:text-gray-200 " href="#">Blog</a></li>
-</ul>
-</nav>
-<div>
-
-{loginVisible && <FormLogin />}
-
-</div>
+        <ul
+          className={`md:hidden px-6 mx-auto font-semibold font-heading  ${
+            menuVisible ? "flex flex-col" : "hidden"
+          }`}
+        >
+          <li>
+            <Link to="/Inmuebles" className="hover:text-gray-200">
+              Inmueble
+            </Link>
+          </li>
+          <li>
+            <a className="hover:text-gray-200 " href="#">
+              Ver corredores
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-gray-200 " href="#">
+              Ver constructoras
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-gray-200 " href="#">
+              Sobre nosotros
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-gray-200 " href="#">
+              Contactos
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-gray-200 " href="#">
+              Blog
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div>{loginVisible && <FormLogin />}</div>
     </div>
-   
-        
-    )
-}
+  );
+};
 
-export { Navbar }
+export { Navbar };
--- Front/src/pages/Inmuebles.jsx
+++ Front/src/pages/Inmuebles.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import {FaRankingStar} from "react-icons/fa6"
 import {FcFactory}  from "react-icons/fc"
 import Carousel from 'react-multi-carousel';
@@ -205,17 +205,13 @@ const Inmuebles = () => {
      
 <div className='flex items-center justify-center h-[9%] mb-[50px] '>
   
-  <a href="/CardsPropiedades"> 
-  <button 
+  <a href="/CardsPropiedades"
     type="button"
-    href="/CardsPropiedades"
     className=" flex justify-center inline-block rounded bg-orange-500 mt-[50px] px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
     data-te-ripple-init
     data-te-ripple-color="light">
    ver todas las solicitudes 
-  </button>
   </a>
-
 </div>
 
 <div className='flex justify-evenly items-center bg-gray-200 h-[50vh]'>
