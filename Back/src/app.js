187bc51f9b79045c01044f303bd85f8929ea0637
Federico Sabiche <fsabiche>
2024-02-15 23:43:15 -0300

    conexion mysql

--- Back/src/app.js
+++ Back/src/app.js
@@ -1,6 +1,9 @@
+
 import express from 'express'
+
 import { pool } from './db.js'
 import { PORT } from './config.js'
+
 
 const app = express()
 
@@ -15,9 +18,12 @@ app.get('/ping', async (req, res) => {
 })
 
 app.get('/create', async (req, res) =>{
-  const result =  await pool.query('INSERT INTO users(name, last_name) VALUES ("doctor", "señor")')
+  const result =  await pool.query('INSERT INTO users(name, last_name) VALUES ("federico ", "sabiche")')
     res.json(result);
 })
 
 app.listen(PORT)
 console.log('Server on port', PORT)
+
+
+
--- Front/src/pages/FormLogin.jsx
+++ Front/src/pages/FormLogin.jsx
@@ -1,11 +1,14 @@
 
-/*
 import { Link } from 'react-router-dom';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const FormLogin = () => {
   const [values, setValues] = useState({});
   console.log(values);
+
+  useEffect(() => {
+    console.log(values);
+  }, [values]); // Este efecto se ejecutará cada vez que 'values' cambie
 
   const handleChangeValues = (value) => {
     setValues((preValue) => ({
@@ -142,133 +145,4 @@ const FormLogin = () => {
 };
 
 export default FormLogin;
-*/
 
-import { Link } from 'react-router-dom';
-import { useState } from "react";
-import { FiLock, FiUser } from "react-icons/fi"; // Importa los iconos necesarios
-
-const FormLogin = () => {
-  const [values, setValues] = useState({});
-  console.log(values);
-
-  const handleChangeValues = (value) => {
-    setValues((preValue) => ({
-      ...preValue,
-      [value.target.name]:[value.target.value],
-    }));
-  };
-
-  const handleClickButton = () =>{
-    console.log(values)
-  }
-
-  return (
-    <div className="flex items-center flex-col mb-10">
-      <div className="max-w-lg mx-auto bg-white p-8 rounded-xl shadow shadow-orange-300 mt-40">
-        <h1 className="text-4xl font-medium mb-3 ">Login</h1>
-        <p className="text-orange-500">
-          Hola, Bienvenido a Mercado Inmobiliario 👋
-        </p>
-
-        <div className="my-5">
-          <button className="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-black-700 hover:border-orange-400 hover:text-orange-700 hover:shadow transition duration-150">
-            {/* Reemplaza la etiqueta <img> con el componente de icono de React */}
-            <FiUser className="w-6 h-6" />{" "}
-            <span>Login con Google</span>
-          </button>
-        </div>
-        <form action="" className="my-10">
-          <div className="flex flex-col space-y-5">
-            <label htmlFor="email">
-              <p className="font-medium text-orange-700 pb-2">
-                Ingrese su Email
-              </p>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-orange-500 hover:shadow"
-                placeholder="Ingrese su Email"
-                onChange={handleChangeValues}
-              />
-            </label>
-            <label htmlFor="password">
-              <p className="font-medium text-orange-700 pb-2">Contraseña</p>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                className="w-full py-3 border border-slate-200 rounded-lg px-3 focus:outline-none focus:border-orange-500 hover:shadow"
-                placeholder="Ingrese su Contraseña"
-                onChange={handleChangeValues}
-              />
-            </label>
-            <div className="flex flex-row justify-between">
-              <div>
-                <label htmlFor="remember" className="">
-                  <input
-                    type="checkbox"
-                    id="remember"
-                    className="w-4 h-4 border-slate-200 focus:ring-orange-700 accent-orange-600"
-                    onChange={handleChangeValues}
-                  />
-                  Recordar Contraseña
-                </label>
-              </div>
-              <div>
-                <Link to={"#"} className="font-medium text-orange-500">
-                  Olvide Contraseña?
-                </Link>
-              </div>
-            </div>
-            <button className="w-full py-3 font-medium text-white bg-orange-600 hover:bg-orange-400 rounded-lg border-orange-400 hover:shadow inline-flex space-x-2 items-center justify-center" onClick={handleClickButton}>
-              {/* Reemplaza la etiqueta <svg> con el componente de icono de React */}
-              <FiLock className="h-6 w-6" />
-              <span>Login</span>
-            </button>
-            <p className="text-center">
-              Todavia no estas Registrado?{" "}
-              <Link
-                to={"/FormUsuario"}
-                className="text-orange-700 font-medium inline-flex space-x-1 items-center"
-              >
-                <span>Registrese Ahora </span>
-                <span>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                    />
-                  </svg>
-                </span>
-              </Link>
-            </p>
-          </div>
-        </form>
-      </div>
-      <div className="flex justify-center">
-        <Link
-          to={"/"}
-          type="button"
-          className=" flex justify-center w-full inlineBlock rounded bg-orange-500 mt-[51px] mb-4
-           px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-          data-te-ripple-init
-          data-te-ripple-color="light"
-        >
-          Volver
-        </Link>
-      </div>
-    </div>
-  );
-};
-
-export default FormLogin;
